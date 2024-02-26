ae2b241b4745c713ec506171d761f9aeaf7e4677
Kiss, Tibor <tibor.kiss01@sap.com>
2024-02-26 17:30:39 +0100

    NVM update

--- scripts/mentor-sql.sh
+++ scripts/mentor-sql.sh
@@ -2,7 +2,7 @@
 
 # Komponens telepites
 cd ~
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.39.5/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.39.7/install.sh | bash
 source ~/.profile
 nvm install 20
 
